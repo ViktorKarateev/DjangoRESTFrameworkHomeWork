55019eb19a8544f1d81d64ea8a5af41883107d2b
karat <karateev.92@gmail.com>
2025-07-10 22:58:40 +0400

    Подключен router в config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from materials.views import CourseViewSet
 
+router = DefaultRouter()
+router.register(r'courses', CourseViewSet, basename='course')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),  #  Добавили router маршруты
     path('api/', include(('materials.urls', 'materials'), namespace='materials')),
     path('api/users/', include(('users.urls', 'users'), namespace='users')),
 ]
--- materials/urls.py
+++ materials/urls.py
@@ -1,14 +1,9 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CourseViewSet, LessonListCreateAPIView, LessonDetailAPIView
-
-router = DefaultRouter()
-router.register(r'courses', CourseViewSet, basename='course')
+from django.urls import path
+from .views import LessonListCreateAPIView, LessonDetailAPIView
 
 app_name = 'materials'
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('lessons/', LessonListCreateAPIView.as_view(), name='lesson-list-create'),
     path('lessons/<int:pk>/', LessonDetailAPIView.as_view(), name='lesson-detail'),
 ]
