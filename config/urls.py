31e9c28c8c34e430b85cd2e99d177a1d7abdefdd
karat <karateev.92@gmail.com>
2025-07-19 14:52:35 +0400

    добавлен permission IsModerator

--- config/urls.py
+++ config/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from materials.views import CourseViewSet
-
-router = DefaultRouter()
-router.register(r'courses', CourseViewSet, basename='course')
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),  #  Добавили router маршруты
-    path('api/', include(('materials.urls', 'materials'), namespace='materials')),
-    path('api/users/', include(('users.urls', 'users'), namespace='users')),
+    path('api/users/', include('users.urls', namespace='users')),
+    path('api/materials/', include('materials.urls', namespace='materials')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
