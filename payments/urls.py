1ff543742737aedd2656b8484c31605c52f53bb4
karat <karateev.92@gmail.com>
2025-07-26 15:28:57 +0400

    добавлен эндпоинт проверки статуса Stripe-сессии

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import CreateStripeSessionView
+from .views import CreateStripeSessionView, StripeSessionStatusAPIView
 
 app_name = 'payments'
 
 urlpatterns = [
     path('create-checkout-session/', CreateStripeSessionView.as_view(), name='create_checkout'),
+    path('check-session-status/', StripeSessionStatusAPIView.as_view(), name='check-session-status'),
 ]
+
+
+
+
+
--- payments/views.py
+++ payments/views.py
@@ -1,9 +1,11 @@
 import stripe
 from django.conf import settings
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework import status
+from .services import retrieve_checkout_session
+
 
 class CreateStripeSessionView(APIView):
     permission_classes = [IsAuthenticated]
@@ -34,3 +36,20 @@ class CreateStripeSessionView(APIView):
             return Response({'session_url': checkout_session.url})
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class StripeSessionStatusAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        session_id = request.query_params.get('session_id')
+
+        if not session_id:
+            return Response({'error': 'session_id is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        result = retrieve_checkout_session(session_id)
+
+        if 'error' in result:
+            return Response(result, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(result, status=status.HTTP_200_OK)
